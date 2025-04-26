1a415a632dfd7038026fba9bd7c3e9bd7ba1a897
Vinicius A. Pereira <vinicius.apereira04@gmail.com>
2025-04-25 21:31:14 -0300

    fix: remove test file

--- src/run_test.py
+++
@@ -1,7 +0,0 @@
-from main import Lexical, Token, TokenType, StringError
-test_file = "../examples/test.pas"
-
-lexer = Lexical(test_file)
-tokens = lexer.tokenize()
-
-print(f"\nTotal tokens encontrados: {len(tokens)}")
